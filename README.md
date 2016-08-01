6a2f3dc71373b0a2fb54436709902e009f1cff51
Held Grijo <shadyueh@gmail.com>
2016-08-01 18:48:08 -0400

    Beginner tips Card Number style

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ SafelyDriving
 SafelyDriving is an app developed as final project for
 the Android Development for Beginners course at Udacity.
 
-It reunites some tips about how to drive motorcycles more safely
+It has cardviews with tips about safely driving motorcycles
+and a quiz to test your knowledge.
+
+Requirements
+---
+Minimum SDK: API 21 (Lollipop 5.0)
--- app/src/main/java/shadowppl/com/br/safelydriving0/BeginnerBikerActivity.java
+++ app/src/main/java/shadowppl/com/br/safelydriving0/BeginnerBikerActivity.java
@@ -1,14 +1,27 @@
 package shadowppl.com.br.safelydriving0;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class BeginnerBikerActivity extends AppCompatActivity {
+
+    public static final String SHARED_PREFS = "SafelyDriveSharedPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beginner_biker);
         setTitle("Beginner Biker");
+    }
+
+    public void markAsReaded(){
+        // All objects are from android.context.Context
+        SharedPreferences sharedPrefs = getSharedPreferences(SHARED_PREFS, 0);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+      //  editor.putBoolean(getString(R.string.saved_high_score), newHighScore);
+
+        // Commit the edits!
+        editor.commit();
     }
 }
