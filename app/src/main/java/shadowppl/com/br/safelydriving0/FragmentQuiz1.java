41d5fee7c0b317a43f8434de92d5d1e17cb7cd78
Held Grijo <shadyueh@gmail.com>
2016-08-02 00:06:59 -0400

    Implementing button Next listener to scroll the fragments

--- app/src/main/java/shadowppl/com/br/safelydriving0/FragmentQuiz1.java
+++ app/src/main/java/shadowppl/com/br/safelydriving0/FragmentQuiz1.java
@@ -3,9 +3,11 @@ package shadowppl.com.br.safelydriving0;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Held on 01/08/2016.
@@ -14,6 +16,25 @@ public class FragmentQuiz1 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_quiz1,container,false);
+
+        //Infla o layout
+        View view = inflater.inflate(R.layout.fragment_quiz1,container,false);
+
+        //Encontra o botao no layout
+        Button btnNext = (Button) view.findViewById(R.id.btnNextQuiz1);
+
+        //Define um listener no botao
+        btnNext.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                //Encontra o ViewPager da Parent
+                ViewPager vPager = (ViewPager) getActivity().findViewById(R.id.viewPager);
+                //Passa para o proximo fragment
+                vPager.setCurrentItem(vPager.getCurrentItem()+1, true);
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/shadowppl/com/br/safelydriving0/FragmentQuiz3.java
+++ app/src/main/java/shadowppl/com/br/safelydriving0/FragmentQuiz3.java
@@ -3,9 +3,11 @@ package shadowppl.com.br.safelydriving0;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Held on 01/08/2016.
@@ -14,6 +16,19 @@ public class FragmentQuiz3 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_quiz3,container,false);
+        View view = inflater.inflate(R.layout.fragment_quiz3,container,false);
+
+        Button btnNext = (Button) view.findViewById(R.id.btnNextQuiz3);
+
+        btnNext.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                ViewPager vPager = (ViewPager) getActivity().findViewById(R.id.viewPager);
+                vPager.setCurrentItem(vPager.getCurrentItem()+1, true);
+            }
+        });
+
+        return view;
     }
 }
